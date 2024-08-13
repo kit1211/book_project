6e6d0ad42efb2c9152013b6d7856f2a17a0b0ff2
kit1211 <117402170+kit1211@users.noreply.github.com>
2024-08-13 15:16:39 +0700

    chore: Update bookRoute.js to use async/await for better error handling

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,7 +3,18 @@ const router        = express.Router();
 const bookService   = require('../services/bookService');
 
 
-router.get('/', (req, res)){
+router.get('/',  
+    async function (req, res, next){
+        try {
+            res.json(
+                await bookService.getMulti(req.query.page)
+            );
+        } catch (error) {
+            console.error(`Error while getting books `, error.message);
+            next(error);
+        }
+    }
+);
 
-}
 
+module.exports = router;

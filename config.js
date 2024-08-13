7cb426f7a4504354ac73780742e15eda40048686
kit1211 <117402170+kit1211@users.noreply.github.com>
2024-08-13 14:54:48 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config ={
+    db : {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "nodejs_learning",
+        connectTimeout: 60000
+    },
+    listPerPage: 10,
+}
+module.exports = config;

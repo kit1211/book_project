2ef026a9e559ff671c9296230c7688308135f324
kit1211 <117402170+kit1211@users.noreply.github.com>
2024-08-13 15:11:54 +0700

    chore: Refactor code for better readability and maintainability

---
+++ routes/bookRoute.js
@@ -0,0 +1,9 @@
+const express       = require('express');
+const router        = express.Router();
+const bookService   = require('../services/bookService');
+
+
+router.get('/', (req, res)){
+
+}
+
--- services/database.js
+++ services/database.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql2/promise');
+const mysql  = require('mysql2/promise');
 const config = require('../config');
 
 
 async function query( sql, params ) {
-    const connection = await mysql.createConnection(config.db);
+    const connection  = await mysql.createConnection(config.db);
     const [results, ] = await connection.execute(sql, params);
 
     return results;
